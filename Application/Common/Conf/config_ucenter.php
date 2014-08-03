082982c6102b26c07d58c2631500f643715544e3
xledoo <xledoo@qq.com>
2014-08-03 22:47:14 +0800

    add conf file

---
+++ Application/Common/Conf/config_ucenter.php
@@ -0,0 +1,51 @@
+<?php
+
+
+// define('UC_CONNECT', 'mysql');
+// define('UC_DBHOST', 'localhost');
+// define('UC_DBUSER', 'root');
+// define('UC_DBPW', '9527');
+// define('UC_DBNAME', 'ucenter');
+// define('UC_DBCHARSET', 'utf8');
+// define('UC_DBTABLEPRE', '`ucenter`.uc_');
+// define('UC_DBCONNECT', '0');
+// define('UC_KEY', 'e90dr3cMXTz/6OZ/2wz+kanEFah8CdQkJMn4Vtc');
+// define('UC_API', 'http://localhost/ucenter');
+// define('UC_CHARSET', 'utf-8');
+// define('UC_IP', '');
+// define('UC_APPID', '1');
+// define('UC_PPP', '20');
+
+// define('UC_CONNECT', 'mysql');
+// define('UC_DBHOST', 'localhost');
+// define('UC_DBUSER', 'root');
+// define('UC_DBPW', '9527');
+// define('UC_DBNAME', 'ucenter');
+// define('UC_DBCHARSET', 'utf8');
+// define('UC_DBTABLEPRE', '`ucenter`.uc_');
+// define('UC_DBCONNECT', '0');
+// define('UC_KEY', '123456');
+// define('UC_API', 'http://localhost/finabao/ucenter');
+// define('UC_CHARSET', 'utf-8');
+// define('UC_IP', '');
+// define('UC_APPID', '1');
+// define('UC_PPP', '20');
+
+
+define('UC_CONNECT', 'mysql');
+define('UC_DBHOST', 'localhost');
+define('UC_DBUSER', 'root');
+define('UC_DBPW', '9527');
+define('UC_DBNAME', 'finabao');
+define('UC_DBCHARSET', 'utf8');
+define('UC_DBTABLEPRE', '`finabao`.uc_');
+define('UC_DBCONNECT', '0');
+define('UC_KEY', 'd5a08ZN+EzigBomuONeW9kKjbSxGVSr9HAc+rvM');
+define('UC_API', 'http://localhost/ucenter');
+define('UC_CHARSET', 'utf-8');
+define('UC_IP', '');
+define('UC_APPID', '1');
+define('UC_PPP', '20');
+
+
+?>
