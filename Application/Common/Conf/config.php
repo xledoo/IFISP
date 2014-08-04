ec4e65443c510bade585b0b9da77c43c73360d65
alipiapia <124910168@qq.com>
2014-08-04 13:25:41 +0800

    Register .htm verification repair

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -1,9 +1,11 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+	'LOGIN_AUTH_NAME'		=>	'finabao',
+	'GLOBAL_AUTH_KEY'		=>	'xR89daRF0fQW3rSv',
+
 	'LAYOUT_ON'				=>	true,
 	'TMPL_CACHE_ON'			=>	false, //关闭模版缓存
-	'GLOBAL_AUTH_KEY'		=>	'xR89daRF0fQW3rSv',
 	'TMPL_TEMPLATE_SUFFIX'  => 	'.htm',     // 默认模板文件后缀
 	'URL_MODEL'             =>	0,
 
@@ -11,11 +13,9 @@ return array(
 	'DB_HOST'   			=> 'localhost', // 服务器地址
 	'DB_NAME'   			=> 'finabao', // 数据库名
 	'DB_USER'   			=> 'root', // 用户名
-	'DB_PWD'    			=> 'root', // 密码
+	'DB_PWD'    			=> '9527', // 密码
 	'DB_PORT'   			=> 3306, // 端口
 	'DB_PREFIX' 			=> 'pre_', // 数据库表前缀
 	'COOKIE_PRE'			=> 'sE8t_',
-
-	'LOGIN_AUTH_NAME'		=>	'finabao'
 
 );
--- Application/Common/Conf/config_ucenter.php
+++ Application/Common/Conf/config_ucenter.php
@@ -32,6 +32,21 @@
 // define('UC_PPP', '20');
 
 
+// define('UC_CONNECT', 'mysql');
+// define('UC_DBHOST', 'localhost');
+// define('UC_DBUSER', 'root');
+// define('UC_DBPW', '9527');
+// define('UC_DBNAME', 'finabao');
+// define('UC_DBCHARSET', 'utf8');
+// define('UC_DBTABLEPRE', '`finabao`.uc_');
+// define('UC_DBCONNECT', '0');
+// define('UC_KEY', 'd5a08ZN+EzigBomuONeW9kKjbSxGVSr9HAc+rvM');
+// define('UC_API', 'http://localhost/ucenter');
+// define('UC_CHARSET', 'utf-8');
+// define('UC_IP', '');
+// define('UC_APPID', '1');
+// define('UC_PPP', '20');
+
 define('UC_CONNECT', 'mysql');
 define('UC_DBHOST', 'localhost');
 define('UC_DBUSER', 'root');
@@ -40,7 +55,7 @@ define('UC_DBNAME', 'finabao');
 define('UC_DBCHARSET', 'utf8');
 define('UC_DBTABLEPRE', '`finabao`.uc_');
 define('UC_DBCONNECT', '0');
-define('UC_KEY', 'd5a08ZN+EzigBomuONeW9kKjbSxGVSr9HAc+rvM');
+define('UC_KEY', '16bfyTX3gggumf58VKGDdL8/cVchROPhv7bspNI');
 define('UC_API', 'http://localhost/ucenter');
 define('UC_CHARSET', 'utf-8');
 define('UC_IP', '');
