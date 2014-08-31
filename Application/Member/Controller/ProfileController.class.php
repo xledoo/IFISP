0c05313c664b8ae26534a96c9de2aa08c610d374
alipiapia <124910168@qq.com>
2014-08-31 12:53:54 +0800

    Update function:myPw

--- Application/Member/Controller/ProfileController.class.php
+++ Application/Member/Controller/ProfileController.class.php
@@ -3,6 +3,8 @@ namespace Member\Controller;
 use Common\Controller\BaseController;
 
 class ProfileController extends BaseController {
+
+    //我的基本资料
     public function index(){
         $gm = $this->_G['member']['mobile'];
         $memb = M('member_profile')->where("mobile='%s'",$gm)->find();
@@ -26,59 +28,64 @@ class ProfileController extends BaseController {
         }
     }
 
-    //密码修改
+    //我的密码
     public function myPw(){
         if(formcheck('edit')){
-            // debug($_POST);
-            loaducenter();
-            $pid = uc_user_edit(I('username'), I('oldpw'), I('newpw'));
-            switch ($pid) {
-                case '0':
-                    $return['msg']  = '没有做任何修改';
-                    $return['err']  =   true;
-                    break;
-                case '-1':
-                    $return['msg']  = '旧密码不正确';
-                    $return['err']  =   true;
-                    break;
-                case '-4':
-                    $return['msg']  = 'Email格式有误';
-                    $return['err']  =   true;
-                    break;
-                case '-5':
-                    $return['msg']  = 'Email不允许注册';
-                    $return['err']  =   true;
-                    break;
-                case '-6':
-                    $return['msg']  = '该Email已被注册';
-                    $return['err']  =   true;
-                    break;
-                case '-7':
-                    $return['msg']  = '没有做任何修改';
-                    $return['err']  =   true;
-                    break;
-                case '-8':
-                    $return['msg']  = '该用户受保护无权限修改';
-                    $return['err']  =   true;
-                    break;
-                default:
-                    $memb = M('member')->where("username='%s'",$_POST['username'])->find();
-                    if($memb){
-                        $data['password']   =   hashmd5($_POST['newpw']);
-                        M('member')->where("username='%s'",$_POST['username'])->save($data);
-                    }
-                    $return['msg'] = '密码修改成功！';
-                    $return['err'] = false;
-                    break;
-            }
-            if($return['err']){
-                $this->error($return['msg']);
-            } else {
-                $this->success($return['msg']);
-            }
+           $this->editPw(I('username'),I('oldpw'),I('newpw'));
         } else {
             $this->assign('formhash', formhash());
             $this->display();            
+        }
+    }
+
+    //密码修改
+    public function editPw($username,  $oldpw,  $newpw,   $email = ''){
+        loaducenter();
+        $pid = uc_user_edit($username,  $oldpw,   $newpw,    $emal);
+        $return['err']  =   false;
+        switch ($pid) {
+            case '0':
+                $return['msg']  = '没有做任何修改';
+                $return['err']  =   true;
+                break;
+            case '-1':
+                $return['msg']  = '旧密码不正确';
+                $return['err']  =   true;
+                break;
+            case '-4':
+                $return['msg']  = 'Email格式有误';
+                $return['err']  =   true;
+                break;
+            case '-5':
+                $return['msg']  = 'Email不允许注册';
+                $return['err']  =   true;
+                break;
+            case '-6':
+                $return['msg']  = '该Email已被注册';
+                $return['err']  =   true;
+                break;
+            case '-7':
+                $return['msg']  = '没有做任何修改';
+                $return['err']  =   true;
+                break;
+            case '-8':
+                $return['msg']  = '该用户受保护无权限修改';
+                $return['err']  =   true;
+                break;
+            default:
+                $memb = M('member')->where("username='%s'",$username)->find();
+                if($memb){
+                    $data['password']   =   hashmd5($newpw);
+                    M('member')->where("username='%s'",$username)->save($data);
+                }
+                $return['msg'] = '密码修改成功！';
+                $return['err'] = false;
+                break;
+        }
+        if($return['err']){
+            $this->error($return['msg']);
+        } else {
+            $this->success($return['msg']);
         }
     }
     
