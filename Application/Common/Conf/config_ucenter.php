30624e244d82491c3dca22abba74ebb79671aaa7
qw <243035210@qq.com>
2014-09-02 11:07:21 +0800

    Update 信用认证中心

--- Application/Common/Conf/config_ucenter.php
+++ Application/Common/Conf/config_ucenter.php
@@ -8,7 +8,7 @@ define('UC_DBNAME', 'finabao');
 define('UC_DBCHARSET', 'utf8');
 define('UC_DBTABLEPRE', '`finabao`.uc_');
 define('UC_DBCONNECT', '0');
-define('UC_KEY', '16bfyTX3gggumf58VKGDdL8/cVchROPhv7bspNI');
+define('UC_KEY', '477akyfFp0s/819+VJur1kfRTBiOIx0R4nYEGIM');
 define('UC_API', 'http://localhost/ucenter');
 define('UC_CHARSET', 'utf-8');
 define('UC_IP', '');
